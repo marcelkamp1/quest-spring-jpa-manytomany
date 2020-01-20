2225ce3fa9c6a7bdfa3bd0fcb686d3145c3b0128
marcelkamp1 <marcelkamp1@gmx.de>
2020-01-20 11:33:01 +0100

    Quest JPA03 done

--- src/main/java/com/wildcodeschool/wildandwizard/entity/Wizard.java
+++ src/main/java/com/wildcodeschool/wildandwizard/entity/Wizard.java
@@ -1,10 +1,16 @@
 package com.wildcodeschool.wildandwizard.entity;
 
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import java.sql.Date;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Wizard {
@@ -19,6 +25,12 @@ public class Wizard {
     private String biography;
     private boolean muggle;
 
+    @ManyToMany
+    @JoinTable(name = "wizard_course",
+    		joinColumns = @JoinColumn(name = "wizard_id"),
+    		inverseJoinColumns = @JoinColumn(name = "course_id")    		)
+    private List<Course> courses = new ArrayList<>();
+    
     public Wizard() {
     }
 
@@ -77,4 +89,13 @@ public class Wizard {
     public void setMuggle(boolean muggle) {
         this.muggle = muggle;
     }
+
+	public List<Course> getCourses() {
+		return courses;
+	}
+
+	public void setCourses(List<Course> courses) {
+		this.courses = courses;
+	}
+    
 }
